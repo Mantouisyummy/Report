8346018e9cda90dbd3a19d3734e53add5d3731b5
Mantou <51238168+Mantou-9487@users.noreply.github.com>
2023-06-23 17:24:12 +0800

    Fix: AttributeError visble

--- core/bot.py
+++ core/bot.py
@@ -84,97 +84,99 @@ class Bot(InteractionBot):
 
         if interaction.data.custom_id == f"report":
             await interaction.response.send_modal(modal=ReportModal())
+        try:
+            if interaction.data.custom_id == f"open_dialogue_{interaction.author.id}":
+                embed = Embed(title="確定要聯繫管理團隊嗎?",colour=Colour.red())
+                components = [
+                    Button(label="我確定!",custom_id=f"confirm_open_dialogue_{interaction.author.id}",style=ButtonStyle.green)
+                ]
+                await interaction.response.send_message(embed=embed,components=components)
+            
+            elif interaction.data.custom_id == f"confirm_open_dialogue_{interaction.author.id}":
+                self.author = interaction.author
+                channel = self.get_channel(1121725584414888059) #之後改掉
+                embed = Embed(title="正在聯繫中....",colour=Colour.light_gray())
+                await interaction.response.edit_message(embed=embed, view=None)
 
-        if interaction.data.custom_id == f"open_dialogue_{interaction.author.id}":
-            embed = Embed(title="確定要聯繫管理團隊嗎?",colour=Colour.red())
-            components = [
-                Button(label="我確定!",custom_id=f"confirm_open_dialogue_{interaction.author.id}",style=ButtonStyle.green)
-            ]
-            await interaction.response.send_message(embed=embed,components=components)
-        
-        elif interaction.data.custom_id == f"confirm_open_dialogue_{interaction.author.id}":
-            self.author = interaction.author
-            channel = self.get_channel(1121725584414888059) #之後改掉
-            embed = Embed(title="正在聯繫中....",colour=Colour.light_gray())
-            await interaction.response.edit_message(embed=embed, view=None)
+                notification_embed = Embed(title="叮咚!",description=f"來自 {interaction.user.name} 的對話請求\n可處理的團隊人員請點底下按鈕以接手請求。",colour=Colour.random())
+                components = [
+                    Button(label="接手",custom_id=f"take_over_dialogue")
+                ]
+                await channel.send(embed=notification_embed,components=components)
 
-            notification_embed = Embed(title="叮咚!",description=f"來自 {interaction.user.name} 的對話請求\n可處理的團隊人員請點底下按鈕以接手請求。",colour=Colour.random())
-            components = [
-                Button(label="接手",custom_id=f"take_over_dialogue")
-            ]
-            await channel.send(embed=notification_embed,components=components)
+            elif interaction.data.custom_id == f"take_over_dialogue":
+                self.admin = interaction.author
+                category = interaction.channel.category
+                overwrites = {
+                    interaction.author: PermissionOverwrite(view_channel=True, read_messages=True),
+                    interaction.guild.me: PermissionOverwrite(view_channel=True, read_messages=True),
+                    interaction.guild.default_role: PermissionOverwrite(view_channel=False)
+                }
+                channel = await interaction.guild.create_text_channel(name=f"dialog-{self.author.id}",category=category, overwrites=overwrites)
+                embed = Embed(title=f"此對話已由 {interaction.user.name} 接手",colour=Colour.green())
+                user = self.get_user(self.author.id)
+                success_embed = Embed(title=f"聯繫成功! 和你對話的團隊人員為: {self.admin.name}",description="⚠️請注意，團隊人員有權隨時關閉此對話，如有任何疑慮或糾紛請至管理團隊所在群組處理。⚠️",colour=Colour.green())
+                await user.send(embed=success_embed)
 
-        elif interaction.data.custom_id == f"take_over_dialogue":
-            self.admin = interaction.author
-            category = interaction.channel.category
-            overwrites = {
-                interaction.author: PermissionOverwrite(view_channel=True, read_messages=True),
-                interaction.guild.me: PermissionOverwrite(view_channel=True, read_messages=True),
-                interaction.guild.default_role: PermissionOverwrite(view_channel=False)
-            }
-            channel = await interaction.guild.create_text_channel(name=f"dialog-{self.author.id}",category=category, overwrites=overwrites)
-            embed = Embed(title=f"此對話已由 {interaction.user.name} 接手",colour=Colour.green())
-            user = self.get_user(self.author.id)
-            success_embed = Embed(title=f"聯繫成功! 和你對話的團隊人員為: {self.admin.name}",description="⚠️請注意，團隊人員有權隨時關閉此對話，如有任何疑慮或糾紛請至管理團隊所在群組處理。⚠️",colour=Colour.green())
-            await user.send(embed=success_embed)
+                await interaction.response.edit_message(embed=embed,view=None)
+                await interaction.followup.send(f"你和對方的對話頻道: {channel.mention}",ephemeral=True)
 
-            await interaction.response.edit_message(embed=embed,view=None)
-            await interaction.followup.send(f"你和對方的對話頻道: {channel.mention}",ephemeral=True)
+                admin_embed = Embed(title="對話管理面板",description="您可以透過底下按鈕來選擇對此對話的執行動作\n※第一次對話時，對方訊息會延遲兩三秒發送為正常現象。※",colour=Colour.random())
+                components = [
+                    Button(label="關閉對話",custom_id=f"close_dialogue_{self.admin.id}",style=ButtonStyle.red),
+                    Button(label="刪除對話",custom_id=f"delate_dialogue_{self.admin.id}",style=ButtonStyle.blurple)
+                ]
+                await channel.send(embed=admin_embed, components=components)
 
-            admin_embed = Embed(title="對話管理面板",description="您可以透過底下按鈕來選擇對此對話的執行動作\n※第一次對話時，對方訊息會延遲兩三秒發送為正常現象。※",colour=Colour.random())
-            components = [
-                Button(label="關閉對話",custom_id=f"close_dialogue_{self.admin.id}",style=ButtonStyle.red),
-                Button(label="刪除對話",custom_id=f"delate_dialogue_{self.admin.id}",style=ButtonStyle.blurple)
-            ]
-            await channel.send(embed=admin_embed, components=components)
+            elif interaction.data.custom_id == f"close_dialogue_{self.admin.id}":
+                await interaction.channel.edit(name=f"closed-dialog-{self.author.id}")
+                close_embed = Embed(title=f"團隊已關閉對話! 祝您有個美好的一天🎉\n如果可以，可以考慮花點時間幫這次對話評分喔!",colour=Colour.red())
 
-        elif interaction.data.custom_id == f"close_dialogue_{self.admin.id}":
-            await interaction.channel.edit(name=f"closed-dialog-{self.author.id}")
-            close_embed = Embed(title=f"團隊已關閉對話! 祝您有個美好的一天🎉\n如果可以，可以考慮花點時間幫這次對話評分喔!",colour=Colour.red())
+                options = [
+                    SelectOption(label="⭐",description="1星",value="⭐"),
+                    SelectOption(label="⭐⭐",description="2星",value="⭐⭐"),
+                    SelectOption(label="⭐⭐⭐",description="3星",value="⭐⭐⭐"),
+                    SelectOption(label="⭐⭐⭐⭐",description="4星",value="⭐⭐⭐⭐"),
+                    SelectOption(label="⭐⭐⭐⭐⭐",description="5星",value="⭐⭐⭐⭐⭐"),
+                ]
+                components = [
+                    StringSelect(placeholder="⭐為這次的對話評分",custom_id="star_rate",max_values=1,min_values=1,options=options)
+                ]
 
-            options = [
-                SelectOption(label="⭐",description="1星",value="⭐"),
-                SelectOption(label="⭐⭐",description="2星",value="⭐⭐"),
-                SelectOption(label="⭐⭐⭐",description="3星",value="⭐⭐⭐"),
-                SelectOption(label="⭐⭐⭐⭐",description="4星",value="⭐⭐⭐⭐"),
-                SelectOption(label="⭐⭐⭐⭐⭐",description="5星",value="⭐⭐⭐⭐⭐"),
-            ]
-            components = [
-                StringSelect(placeholder="⭐為這次的對話評分",custom_id="star_rate",max_values=1,min_values=1,options=options)
-            ]
+                user = self.get_user(self.author.id)
+                await user.send(embed=close_embed,components=components)
 
-            user = self.get_user(self.author.id)
-            await user.send(embed=close_embed,components=components)
+                conversation_record = await interaction.channel.history(limit=None).flatten()  # 讀取頻道的歷史訊息
+                text = ""  # 將字串變為空
+                for message in conversation_record:  # 利用迴圈讀取歷史訊息
+                    if (
+                        message.author.id != interaction.guild.me.id
+                        and message.content != ""
+                    ):  # 排除機器人的訊息和內容空白
+                        now = message.created_at.astimezone(
+                            timezone(offset=timedelta(hours=8))
+                        )  # 將時區變為台灣時區
+                        text = f"{now.year}/{now.month}/{now.day} {now.hour}:{now.minute} - {message.author.display_name}: {message.content}\n{text}"
+                with open(f"chat.txt", "w", encoding="utf-8") as chat:
+                    chat.write(text)  # 將訊息寫入至chat.txt
+                components = [
+                    Button(label="刪除對話",custom_id=f"delate_dialogue_{self.admin.id}",style=ButtonStyle.blurple)
+                ]
+                await interaction.channel.send("對話紀錄:",file=File("chat.txt"),components=components)
+                os.remove("chat.txt")
 
-            conversation_record = await interaction.channel.history(limit=None).flatten()  # 讀取頻道的歷史訊息
-            text = ""  # 將字串變為空
-            for message in conversation_record:  # 利用迴圈讀取歷史訊息
-                if (
-                    message.author.id != interaction.guild.me.id
-                    and message.content != ""
-                ):  # 排除機器人的訊息和內容空白
-                    now = message.created_at.astimezone(
-                        timezone(offset=timedelta(hours=8))
-                    )  # 將時區變為台灣時區
-                    text = f"{now.year}/{now.month}/{now.day} {now.hour}:{now.minute} - {message.author.display_name}: {message.content}\n{text}"
-            with open(f"chat.txt", "w", encoding="utf-8") as chat:
-                chat.write(text)  # 將訊息寫入至chat.txt
-            components = [
-                Button(label="刪除對話",custom_id=f"delate_dialogue_{self.admin.id}",style=ButtonStyle.blurple)
-            ]
-            await interaction.channel.send("對話紀錄:",file=File("chat.txt"),components=components)
-            os.remove("chat.txt")
-
-        elif interaction.data.custom_id == f"delate_dialogue_{self.admin.id}":
-            await interaction.response.send_message("正在刪除....")
-            await asyncio.sleep(1)
-            await interaction.channel.delete()
-        
-        elif interaction.data.custom_id == f"star_rate":
-            value = interaction.data.values[0]
-            await interaction.response.edit_message(f"感謝你的評分🎉🎉🎉 你評了: {value}",view=None, embed=None)
-            channel = self.get_channel(1121725584414888059) #之後改掉
-            embed = Embed(title=f"{self.author.name} 的對話評分",description=f"星數:\n{value}",colour=Colour.yellow())
-            await channel.send(embed=embed)
+            elif interaction.data.custom_id == f"delate_dialogue_{self.admin.id}":
+                await interaction.response.send_message("正在刪除....")
+                await asyncio.sleep(1)
+                await interaction.channel.delete()
+            
+            elif interaction.data.custom_id == f"star_rate":
+                value = interaction.data.values[0]
+                await interaction.response.edit_message(f"感謝你的評分🎉🎉🎉 你評了: {value}",view=None, embed=None)
+                channel = self.get_channel(1121725584414888059) #之後改掉
+                embed = Embed(title=f"{self.author.name} 的對話評分",description=f"星數:\n{value}",colour=Colour.yellow())
+                await channel.send(embed=embed)
+        except AttributeError:
+            pass
 
 
